15374472be2f663175236fceb2d8738e2541f4cb
gouravkotnala <gouravkotnala777@gmail.com>
2025-04-23 22:16:34 +0530

    feat(constants.ts & utils.scss) : add color constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,8 @@
 
 
 export const BG_COLOR = "#eafff5";
+export const PRIMARY_FADE = "#c5ffec87";
+export const PRIMARY_LIGHTER = "#e0fff5";
 export const PRIMARY_LIGHT = "#c5ffec";
 export const PRIMARY_DARK = "#7dffd4";
 export const FONT_PRIMARY = "#5b766d";
