5a1f9b7ee985cf409db5a7186d8b671fee670749
gouravkotnala <gouravkotnala777@gmail.com>
2025-04-27 18:36:51 +0530

    feat(App.tsx) : set login user context state & remove ProtectedRoute 'Child' prop

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,26 @@ import Agents from "./pages/Agents.tsx";
 import Header from "./components/Header.tsx";
 import CreateFormPanel from "./components/CreateFormPanel.tsx";
 import SingleSite from "./pages/SingleSite.tsx";
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
+import { useSelectedRoute } from "./Context.tsx";
+import { useEffect } from "react";
+import { myProfile } from "./api.ts";
 
 
 function App() {
-  //const [selectedRoute, setSelectedRoute] = useState<RoutesTypes|null>(null);
+  const {loginUser, setLoginUser} = useSelectedRoute();
 
-
+  useEffect(() => {
+    myProfile()
+    .then((myProfileRes) => {
+      if (myProfileRes.success) {
+        setLoginUser(myProfileRes.jsonData);     
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }, []);
 
   return (
     <>
@@ -29,19 +43,18 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/clients" element={<Clients />} />
-          <Route path="/agents" element={<Agents />} />
-          {/*<Route path="/plots" element={<Plots />} />*/}
-          <Route path="/slips" element={<Slips />} />
-          <Route path="/sites" element={<Sites />} />
+          <Route path="/" element={<ProtectedRoute ChildElement={<Home />} userRole={loginUser?.role} />} />
+          <Route path="/home" element={<ProtectedRoute ChildElement={<Home />} userRole={loginUser?.role} />} />
+          <Route path="/clients" element={<ProtectedRoute ChildElement={<Clients />} userRole={loginUser?.role} />} />
+          <Route path="/agents" element={<ProtectedRoute ChildElement={<Agents />} userRole={loginUser?.role} />} />
+          <Route path="/slips" element={<ProtectedRoute ChildElement={<Slips />} userRole={loginUser?.role} />} />
+          <Route path="/sites" element={<ProtectedRoute ChildElement={<Sites />} userRole={loginUser?.role} />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/me" element={<MyProfile />} />
-          <Route path="/single-plot" element={<SinglePlot />} />
-          <Route path="/single-site" element={<SingleSite />} />
-          <Route path="/create" element={<CreateFormPanel />} />
+          <Route path="/me" element={<ProtectedRoute ChildElement={<MyProfile />} userRole={loginUser?.role} />} />
+          <Route path="/single-plot" element={<ProtectedRoute ChildElement={<SinglePlot />} userRole={loginUser?.role} />} />
+          <Route path="/single-site" element={<ProtectedRoute ChildElement={<SingleSite />} userRole={loginUser?.role} />} />
+          <Route path="/create" element={<ProtectedRoute ChildElement={<CreateFormPanel />} userRole={loginUser?.role} />} />
         </Routes>
       </BrowserRouter>
     </>
