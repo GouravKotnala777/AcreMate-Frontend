47ac3d47ed508a18b3801b2a9c45c3808df97185
gouravkotnala <gouravkotnala777@gmail.com>
2025-05-03 11:24:03 +0530

    perf(utilFunctions.ts) : add 'getDateByString' utility function

--- src/utils/utilFunctions.ts
+++ src/utils/utilFunctions.ts
@@ -1,7 +1,7 @@
 
 
 
-export const getMonthsCovered = (createdAt?:Date) => {
+export const getMonthsCovered = (createdAt?:string) => {
     if (!createdAt) return 0;
     const startingDate = new Date(createdAt);
     const currentDate = new Date();
@@ -10,4 +10,8 @@ export const getMonthsCovered = (createdAt?:Date) => {
     const monthDiff = currentDate.getMonth() - startingDate.getMonth();
 
     return yearDiff*12+monthDiff+1;
+};
+
+export const getDateByString = (date?:string) => {
+    return new Date(date as string).toLocaleDateString("en-GB");
 }
